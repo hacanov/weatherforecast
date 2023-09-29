7ca02758080092c459dde1d6901153a6e705e3f5
hacanov <123230509+hacanov@users.noreply.github.com>
2023-09-29 23:24:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const API = 'a0b4e5c82a2a812bf0a263a083b1733f';
+window.addEventListener('load', () =>{
+  let long;
+  let lat;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+          const { sunrise, sunset } = data.sys;
+        });
+    });
+  }
+});
